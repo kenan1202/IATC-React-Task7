864048b9ae4d9d4aa61446a9896c8c7bd37fdb04
kenan1202 <kenan.tanriverdi.1@gmail.com>
2022-04-25 19:43:18 +0400

    some changes to list

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -12,7 +12,7 @@ function ListItem({item}) {
         <div className='listItem'>
             <img src = {item.image} alt = "Item Image" width = "50px"></img>
             <h2>{item.name}</h2>
-            <p>{item.price}</p>
+            <p>{Math.floor(item.price)}</p>
             <button onClick={() => dispatch({type: 'ADD_PRICE', payload: item})}>Increase Price</button>
             <button onClick={() => dispatch({type: 'REMOVE_ITEM', payload: item})}>Remove Item</button>
         </div>
